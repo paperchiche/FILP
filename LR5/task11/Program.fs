c78ba1f67128992961e0c2a3f9e45494fb0794a5
paperchiche <20artyom02@mail.ru>
2022-04-04 16:43:50 +0300

    5.11

---
+++ LR5/task11/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let Answer A =
+    match A with
+    | "F#" | "Prolog" -> System.Console.WriteLine("Вы подлиза") 
+    | _ -> System.Console.WriteLine("Проверка пройдена") 
+
+
+[<EntryPoint>]
+let main argv =
+    System.Console.WriteLine("Какой Ваш любимый язык программирования?")
+    let B = Console.ReadLine()
+    Answer B
+    0
