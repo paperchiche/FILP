2b14f213444c527254c611795e3e1a9e834787c4
paperchiche <20artyom02@mail.ru>
2022-04-04 16:48:16 +0300

    5.12

---
+++ LR5/task12/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let Answer (lang:string) = 
+        match lang.Trim() with
+        |"F#" | "Prolog" -> "Вы подлиза"
+        |"C++" -> "Может быть, подумаете ещё?"
+        |"C#" -> "Может быть, подумаете ещё?"
+        |"Python" -> "Может быть, подумаете ещё?"
+        |"Pascal" -> "Неправильный ответ"
+        |_ -> "Бог Вам судья"
+
+    // Суперпозиция
+    Console.WriteLine("Какой Ваш любимый язык программирования?")
+    (Console.ReadLine >> Answer >> Console.WriteLine)()
+    
+    // Каррирование
+    Console.WriteLine("Какой Ваш любимый язык программирования?")
+    let result Answer func out = out(func(Answer()))
+    result Console.ReadLine Answer Console.WriteLine
+  
+    0
