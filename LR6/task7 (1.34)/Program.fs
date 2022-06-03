97bef02d182cac5c86d1ad9b299a1e7e7baf093b
paperchiche <20artyom02@mail.ru>
2022-06-03 11:45:12 +0300

    task7 (1.34)

---
+++ LR6/task7 (1.34)/Program.fs
@@ -0,0 +1,40 @@
+﻿// 1.34 Дан целочисленный массив и отрезок a..b. Необходимо найти элементы, значение которых принадлежит этому отрезку.
+
+open System
+
+let init_list() = 
+    let rec read_list n=
+        if n=0 then []
+        else
+            let Head = Convert.ToInt32(Console.ReadLine())
+            let Tail = read_list (n-1)
+            Head::Tail
+    Console.WriteLine("Введите количество элементов: ")
+    let a = read_list (Convert.ToInt32(Console.ReadLine()))
+    a
+
+let rec write_list = function
+    [] -> let z = Console.ReadKey()
+          0
+    |   (head:int)::tail -> 
+                   Console.WriteLine(head)
+                   write_list tail  
+
+let func list a b =
+    let rec func_rec list a b newList=
+        match list with
+        |[]->newList
+        |h::t->
+            if (h>=a)&&(h<=b) then func_rec t a b newList@[h]
+            else func_rec t a b newList
+    func_rec list a b []
+
+[<EntryPoint>]
+let main argv =
+    let list = init_list()
+    Console.WriteLine("Введите значения отрезка: ")
+    let a = Convert.ToInt32(Console.ReadLine())
+    let b = Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Элементы, принадлежащие заданному отрезку: ")
+    func list a b|>Console.WriteLine
+    0 // return an integer exit code
