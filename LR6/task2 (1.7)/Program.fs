dcf8014fb13e1b86bd928ceb64fce8e99ba746d0
paperchiche <20artyom02@mail.ru>
2022-06-03 03:48:40 +0300

    task2 (1.7)

---
+++ LR6/task2 (1.7)/Program.fs
@@ -0,0 +1,33 @@
+﻿(*Дан целочисленный массив. Необходимо осуществить
+циклический сдвиг элементов массива вправо на две позиции.*)
+
+let leng list = 
+    let rec loop list length =
+        match list with
+        |head::tail ->
+            let nextLength = length + 1
+            loop tail nextLength
+        |[] -> length
+    loop list 0
+    
+let shift list =
+    let rec prom list acc_list listlen = 
+        match list with 
+        |[] -> acc_list
+        |head::tail ->
+            let new_acc_list = if listlen > 0 then (acc_list @ [head]) else (if listlen <> -1 then list@acc_list else acc_list)
+            let new_listlen= listlen - 1
+            prom tail new_acc_list new_listlen
+    prom list [] (leng list - 2)
+let rec print_l =
+    function
+    | [] -> ()
+    | headch::tails -> 
+        printf "%O " headch 
+        print_l tails 
+
+[<EntryPoint>]
+let main argv =
+    let l = [1;3;3;1;5;6]
+    shift l |> print_l
+    0
